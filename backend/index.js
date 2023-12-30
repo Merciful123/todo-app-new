3f891843c417a0fbbb2c3f1dcdb21549a7971830
Aamir Raza <merciful9795@gmail.com>
2023-12-30 10:43:27 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import mysql from "mysql";
+
+const app = express()
+
+const port = 8000
+
+const db = mysql.createPool({
+    host: "localhost",
+    user:"root",
+    password: "Mysql@313",
+    database: "todo-test",
+    
+})
+
+ 
+
+app.get("/", (req, res) => {
+    res.send("This is our backend")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+
+    db.query(q,(err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on ${port}`)
+})
